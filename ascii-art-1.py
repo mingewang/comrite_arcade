60bc5164dc3a3a72e29b140b7f25ca53c3dab54f
Min Wang <mingewang@gmail.com>
2023-08-26 12:47:17 -0500

    Add more examples for ascii-art

--- ascii-art-1.py
+++ ascii-art-1.py
@@ -5,5 +5,17 @@ init(strip=not sys.stdout.isatty()) # strip colors if stdout is redirected
 from termcolor import cprint 
 from pyfiglet import figlet_format
 
+# use pyfiglet -l to get fonts
+cprint("hello world", 'blue', 'on_white')
 cprint(figlet_format('missile!', font='starwars'),
+       'yellow', 'on_red', attrs=['bold'])
+
+
+cprint(figlet_format('missile!', font='slant'),
+       'yellow', 'on_red', attrs=['bold'])
+
+cprint(figlet_format('missile!', font='3-d'),
+       'yellow', 'on_red', attrs=['bold'])
+
+cprint(figlet_format('missile!', font='alphabet'),
        'yellow', 'on_red', attrs=['bold'])
