60d3b729242b5643ee703354bf2bd4b95ce8396b
Min Wang <mingewang@gmail.com>
2023-09-30 16:06:50 -0500

    two ways of import modules in the package

--- module_examples/my_module/__init__.py
+++ module_examples/my_module/__init__.py
@@ -2,8 +2,17 @@
 print(f'Invoking __init__.py for {__name__}') 
 MY_CONSTS = ['A', 'B', 'C']
 
+# https://docs.python.org/3/tutorial/modules.html
+# user need to use from my_module import * as convention
+# system will import all modules
+# just import my_mdoule will not work
+__all__ = [ "my_greet", "my_greet2"]
+
+# the following works 
+# import my_module
 #import my_greet
 import my_module.my_greet as my_greet
+
 #this does NOT work, always complains
 # ModuleNotFoundError: No module named 'my_greet2'
 # import my_greet2
