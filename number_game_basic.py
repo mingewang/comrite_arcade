ac01b5bf568cedcd1f308d291e23c8abd6e31861
Min Wang <mingewang@gmail.com>
2024-01-13 10:26:07 -0600

    add basic number guess game

---
+++ number_game_basic.py
@@ -0,0 +1,14 @@
+random_number = 23
+print("Welcome to the Number Guessing Game!")
+print("Guess the number between 1 and 100. ")
+
+while True:
+    guess = int(input("Enter your guess: "))
+    if guess < random_number:
+        print("Too low! Try a higher number.")
+    elif guess > random_number:
+        print("Too high! Try a lower number.")
+    else:
+        break
+
+print("Congratulations! ")
