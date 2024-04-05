17c3986df2e373da5794f853090de6d97fb352bc
Min Wang <mingewang@gmail.com>
2024-04-05 18:42:18 -0500

    add basic neighbor example

---
+++ list_set_dict_examples/strongest_neighbor_basic.py
@@ -0,0 +1,40 @@
+'''
+original at:
+https://www.geeksforgeeks.org/python-program-to-find-the-strongest-neighbour/
+
+only compare right neighbor.
+
+Given an array arr[] of N positive integers. The task is to find the maximum for every adjacent pair in the array.
+Examples:
+
+Input: 1 2 2 3 4 5
+Output:2 3 4 5 5
+
+'''
+def maximumAdjacent(arr1):
+    # length of arr1
+    n = len(arr1)
+      # array to store the max 
+    # value between adjacent pairs
+    arr2 = []  
+
+    if n <= 1:
+        arr2 = arr1
+     
+    # iterate from 0 to n - 1
+    for i in range(0, n-1):
+        # initial max as this element
+        max = arr1[i]
+        #print("i is: ", i,  " n is: ", n)
+        if max < arr1[i+1]:
+            max = arr1[i+1]
+        arr2.append(max)    
+
+    return arr2
+         
+if __name__ == "__main__" :
+   
+  # input array
+  arr1 = [1,2,2,3,4,5]
+  result = maximumAdjacent(arr1)
+  print("result is: ", result)
