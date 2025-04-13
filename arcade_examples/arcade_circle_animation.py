4992a183b2c1912df1fb7cb3df1ca58283aef2bd
Min Wang <mingewang@gmail.com>
2025-04-13 15:46:52 -0500

    update to use self.clear()

--- arcade_examples/arcade_circle_animation.py
+++ arcade_examples/arcade_circle_animation.py
@@ -18,7 +18,8 @@ class MyGame(arcade.Window):
 
     def on_draw(self):
         """ Called whenever we need to draw the window. """
-        arcade.start_render()
+        #arcade.start_render()
+        self.clear() 
 
         arcade.draw_circle_filled(self.ball_x, self.ball_y, 15, arcade.color.AUBURN)
         # you will see this is also 1/60 second
@@ -30,6 +31,8 @@ class MyGame(arcade.Window):
         """ Called to update our objects. Happens approximately 60 times per second."""
         self.ball_x += 1
         self.ball_y += 1
+        # you will see this is also 1/60 second
+        #print("update time is: ", time.time() )
         # you will see this is 1/60 second
         #print("time is: ", delta_time)
 
