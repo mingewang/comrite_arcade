4872c2805306cd66dfd3b7b05a09b198cd4990cd
Min Wang <mingewang@gmail.com>
2025-04-13 10:21:49 -0500

    update to use self.clear()

--- arcade_examples/arcade_circle.py
+++ arcade_examples/arcade_circle.py
@@ -8,10 +8,14 @@ class MyGame(arcade.Window):
     super().__init__(width, height, title)
     # Set the background color
     arcade.set_background_color(arcade.color.ASH_GREY)
+    #arcade.set_background_color(arcade.color.BALL_BLUE)
 #
+  # roughly called 60 times /second
   def on_draw(self):
     """ Called whenever we need to draw the window. """
-    arcade.start_render()
+    # It clears the screen and fills it with the background color set by arcade.set_background_color().
+    # This prepares the screen for the next frame's drawing operations.
+    self.clear() 
     arcade.draw_circle_filled(50, 50, 15, arcade.color.AUBURN)
     arcade.draw_rectangle_filled(150,150,60, 60, arcade.color.ALLOY_ORANGE)
     arcade.draw_ellipse_filled(180,180, 40, 150, arcade.color.AMARANTH)
